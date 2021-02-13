2be567104c907812a0d981576be76cd047e64bc5
Bryan Hoang <bryan.c.hoang@gmail.com>
2021-02-13 17:08:51 -0500

    test: remove tests

--- test/index.test.ts
+++
@@ -1,7 +0,0 @@
-import helloWorld from '../src/index.ts';
-
-test('Hello World', () => {
-  console.log = jest.fn();
-  helloWorld();
-  expect(console.log).toHaveBeenCalledWith('Hello world!');
-});
