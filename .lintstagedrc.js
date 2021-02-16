aefc22855c627e63a3e1b460e192b2a3a5b42be3
Bryan Hoang <bryan.c.hoang@gmail.com>
2021-02-15 19:08:13 -0500

    ci: fix scripts to get ci working

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,15 +1,16 @@
-const typeCheckCommand = () => 'npm run check:types --if-present';
+const typeCheckCommand = () => 'npm run check:types';
 const eslintCommand = 'npm run lint -- --fix';
 const prettierCommand = 'npm run format -- --write';
 const markdownlintCommand = 'npm run lint:markdown -- --fix';
+const spellCheckCommand = 'npm run check:spelling';
 
 const micromatchFileExtension = (...fileExtensions) =>
   `*.(${fileExtensions.join('|')})`;
 
 module.exports = {
-  [micromatchFileExtension('ts', 'tsx')]: [typeCheckCommand],
-  [micromatchFileExtension('ts', 'tsx', 'js')]: [eslintCommand],
-  [micromatchFileExtension('md')]: [markdownlintCommand],
+  [micromatchFileExtension('ts')]: [typeCheckCommand],
+  [micromatchFileExtension('ts', 'js')]: [eslintCommand, spellCheckCommand],
+  [micromatchFileExtension('md')]: [markdownlintCommand, spellCheckCommand],
   [micromatchFileExtension('md', 'json', 'svg', 'yaml', 'yml')]: [
     prettierCommand,
   ],
